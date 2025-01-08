a4a4a5626241996643bd05d67c9953c17a575518
yagokoki <yagokoki@gmail.com>
2025-01-08 10:45:18 -0300

    estilizando newproject e adicionando o formulário de projeto novo

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,27 @@
+function ProjectForm() {
+  return (
+      <form>
+        <div>
+            <input type="text" placeholder="Insira o nome do Projeto" />
+        </div>
+
+        <div>
+            <input type="number" placeholder="Insira o orçamento total" />
+        </div>
+
+
+        <div>
+            <select name='category_id'>
+                <option disabled>Selecione a categoria</option>
+            </select>
+        </div>
+
+        <div>
+            <input type="submit" value="Criar Projeto" />
+        </div>
+
+      </form>
+  );
+}
+
+export default ProjectForm;
