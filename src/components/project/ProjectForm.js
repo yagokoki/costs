26713a2bf3c1371f1ad6785731a74ee3fa3d1e50
yagokoki <yagokoki@gmail.com>
2025-01-08 11:34:09 -0300

    Componentização e estilização do formulário

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,24 +1,18 @@
-function ProjectForm() {
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+import styles from './ProjectForm.module.css';
+
+function ProjectForm({btnText}) {
   return (
-      <form>
-        <div>
-            <input type="text" placeholder="Insira o nome do Projeto" />
-        </div>
+      <form className={styles.form}>
+        <Input type="text" text="Nome do Projeto:" name="name" placeholder="Insira o nome do projeto" />
 
-        <div>
-            <input type="number" placeholder="Insira o orçamento total" />
-        </div>
+        <Input type="number" text="Orçamento do projeto::" name="budget" placeholder="Insira o orçamento total" />
 
+        <Select name="category_id" text="Selecione uma categoria:" />
 
-        <div>
-            <select name='category_id'>
-                <option disabled>Selecione a categoria</option>
-            </select>
-        </div>
-
-        <div>
-            <input type="submit" value="Criar Projeto" />
-        </div>
+        <SubmitButton text={btnText}/>
 
       </form>
   );
