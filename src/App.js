894496e9bd59b1537d29f457f58a0d3b48263a6a
yagokoki <yagokoki@gmail.com>
2025-01-10 14:36:21 -0300

    adicionada a página singular para cada projeto

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
+
 import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import Projects from "./components/pages/Projects";
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<Project />} />
+
         </Routes>
       </Container>
 
